bd70d1034c52d3bab9c5f01f34b22bb192b01ef6
Radhika104 <87032257+Radhika104@users.noreply.github.com>
2023-05-19 20:53:17 +0530

    assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,34 +1,27 @@
 package com.driver;
-class Product
-{
-public int x;
-public int y;
-public int z;
-private double a;
-private double b;
-void product(int x,int y)
-{
-this.x=x;
-this.y=y;
-}
-void product(int x,int y,int z)
-{
-this.x=x;
-this.y=y;
-this.z=z;
-}
-void product(double x,double y)
-{
-this.a=x;
-this.b=y;
-}
-}
+
 public class Main {
+public static class Product
+{
+int product(int x,int y)
+{
+return x*y;
+}
+int product(int x,int y,int z)
+{
+return x*y*z;
+}
+double product(double x,double y)
+{
+return x*y;
+}
+}
 public static void main(String args[])
 {
 Product p=new Product();
-p.product(10,20);
-p.product(10,20,30);
-p.product(10.0,20.0);
+int ans=p.product(10,20);
+int ans1=p.product(10,20,30);
+double ans2=p.product(10.0,20.0);
+System.out.println(ans+" "+ans1+" "+ans2);
 }
 }
